58f76a8a2032aea8841c9f564001d279bd40a047
Vipul Pareek <vipulpareek2003@gmail.com>
2026-02-28 10:39:53 +0530

    update pom.xml urls

---
+++ StudentUtils/src/main/java/io/github/workVipulPareek/StudentUtils.java
@@ -0,0 +1,8 @@
+package io.github.workVipulPareek;
+
+public class StudentUtils {
+
+    public static String greet(String name) {
+        return "Hello " + name + ", published from Maven Central!";
+    }
+}
