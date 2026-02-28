8a9048a09c481c3cd21b18fab95e1c80667a86cb
Vipul Pareek <vipulpareek2003@gmail.com>
2026-02-28 10:21:33 +0530

    mavenRepo Project

---
+++ StudentUtils/src/main/java/io/github/vipulpareek/StudentUtils.java
@@ -0,0 +1,8 @@
+package io.github.vipulpareek;
+
+public class StudentUtils {
+
+    public static String greet(String name) {
+        return "Hello " + name + ", welcome to Maven Central!";
+    }
+}
