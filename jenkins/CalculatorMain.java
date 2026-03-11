4c792ddc20209ca9015491dfa1361e42a1c91cee
Vipul Pareek <vipulpareek2003@gmail.com>
2026-03-11 17:03:18 +0530

    jenkins assignment done

---
+++ jenkins/CalculatorMain.java
@@ -0,0 +1,15 @@
+public class CalculatorMain {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(10,5));
+        System.out.println("Subtraction: " + calc.sub(10,5));
+        System.out.println("Multiplication: " + calc.mul(10,5));
+        System.out.println("Division: " + calc.div(10,5));
+
+        System.out.println("Pipeline Testing!");
+
+    }
+}
