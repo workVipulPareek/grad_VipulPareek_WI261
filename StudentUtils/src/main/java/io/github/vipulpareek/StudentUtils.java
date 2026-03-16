72afbff172cb51879bd649517b162ddce9fbce98
Vipul Pareek <vipulpareek2003@gmail.com>
2026-03-16 16:26:02 +0530

    HTML files uploaded

--- StudentUtils/src/main/java/io/github/vipulpareek/StudentUtils.java
+++
@@ -1,8 +0,0 @@
-package io.github.vipulpareek;
-
-public class StudentUtils {
-
-    public static String greet(String name) {
-        return "Hello " + name + ", welcome to Maven Central!";
-    }
-}
--- StudentUtils/src/main/java/io/github/workVipulPareek/StudentUtils.java
+++
@@ -1,8 +0,0 @@
-package io.github.workVipulPareek;
-
-public class StudentUtils {
-
-    public static String greet(String name) {
-        return "Hello " + name + ", published from Maven Central!";
-    }
-}
